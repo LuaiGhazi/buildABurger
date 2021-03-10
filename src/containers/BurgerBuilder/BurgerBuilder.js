dbd3509c0519a5676c9c14f6f4e51897e501d0bd
LuaiGhazi <luaighazi@gmail.com>
2021-03-10 10:37:22 -0800

    Added the Burger Component and styling for that component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import Burger from '../../components/Burger/Burger'
 
 // Used a state based component here because it'll be a stateful component
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <>
-                <div> Burger</div>
+                <Burger />
                 <div> Build Controls</div>
             </>
         );
