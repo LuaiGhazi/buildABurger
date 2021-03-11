152640711cea2a9b51f61aad6c4d75bb655ad37a
LuaiGhazi <luaighazi@gmail.com>
2021-03-11 15:18:19 -0800

    Toolbar added

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button'
 // import classes from './Modal.module.css'
 
 
@@ -18,9 +19,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong> Total Price: {props.price.toFixed(2)} </strong></p>
             <p> Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </>
     );
 };
