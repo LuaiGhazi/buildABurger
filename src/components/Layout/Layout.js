b85451a4193f9ee2878ef60a6b29de95b866a26e
LuaiGhazi <luaighazi@gmail.com>
2021-03-12 11:26:14 -0800

    Toolbar functionality, sidebar, and styling added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import { Component } from 'react';
 
-const layout = (props) => (
-    <>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
-);
+class layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
+};
+
 
 export default layout
