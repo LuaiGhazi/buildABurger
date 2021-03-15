90c1c20c24b73f145ad633aae2dce773a25f3ac8
LuaiGhazi <luaighazi@gmail.com>
2021-03-14 18:20:47 -0700

    Performance improvement. Added lifecycle hook to check if component should update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button'
 // import classes from './Modal.module.css'
 
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}:</span> {props.ingredients[igKey]}
-                </li>);
-        });
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log()
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}:</span> {this.props.ingredients[igKey]}
+                    </li>);
+            });
 
-    return (
-        <>
-            <h3>Your Order</h3>
-            <p> A delicious burger with the following ingredients?</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong> Total Price: {props.price.toFixed(2)} </strong></p>
-            <p> Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </>
-    );
-};
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p> A delicious burger with the following ingredients?</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong> Total Price: {this.props.price.toFixed(2)} </strong></p>
+                <p> Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </>
+        )
+    }
+}
 
-export default orderSummary;
+
+export default OrderSummary;
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import classes from './NavigationItem.module.css'
 
 const navigationItem = (props) => (
