3e5ffadd9a91aa6addf475717df17593a6e349de
Luai Ghazi <luaighazi@gmail.com>
2021-03-31 17:56:01 -0700

    Bug fix related to authentication

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -66,7 +66,7 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
     }
 
     switchAuthModeHandler = () => {
